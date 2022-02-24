1ce573c7aa6e0127060311a75fe7999486d30195
felipe cembranelli <feliperc@ciandt.com>
2022-02-24 16:46:28 -0500

    changed coin name to myCoin

--- src/components/web3Wrapper/web3Wrapper.jsx
+++ src/components/web3Wrapper/web3Wrapper.jsx
@@ -22,9 +22,6 @@ class Web3Wrapper extends Component {
 
 	
 	async componentDidMount() {
-		//this.connectWalletHandler();
-
-		//await this.checkDAIBalance()
 
 		try {
 			//window.ethereum.on('chainChanged', this.chainChangedHandler);
@@ -37,24 +34,6 @@ class Web3Wrapper extends Component {
 	}
 
 	async componentWillMount() {
-
-		// if (window.ethereum) {
-		// 	window.web3 = new Web3(window.ethereum)
-		// 	await window.ethereum.enable()
-		//   }
-		//   else if (window.web3) {
-		// 	window.web3 = new Web3(window.web3.currentProvider)
-		//   }
-		//   else {
-		// 	window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-		//   }
-
-		// const web3 = window.web3
-
-		// const networkId = await web3.eth.net.getId()
-
-    	//console.log("componentWillMount - networkId:" + networkId)
-
 
 		await this.connectWalletHandler()
 	}
